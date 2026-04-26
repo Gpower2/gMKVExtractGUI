c8d532eedcb6f4fa3a40b789a98d68f43be0e548
Arslanoglou Georgios <arslanoglou.georgios@gmail.com>
2026-04-27 00:48:43 +0300

    Bump version to 2.13.0

--- src/gMKVToolNix.Translator.Console/Properties/AssemblyInfo.cs
+++ src/gMKVToolNix.Translator.Console/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("gMKVToolNix.Translator.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gpower2")]
 [assembly: AssemblyProduct("gMKVToolNix.Translator.Console")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright © 2014-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.13.0.0")]
+[assembly: AssemblyFileVersion("2.13.0.0")]
